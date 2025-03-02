675808d6ba4fbf36e51000ae27c9ad1dbf82fd6c
qSlxyed <158263450+qSlxyed@users.noreply.github.com>
2025-03-02 22:31:33 +0600

    calculator.py

--- calculator.py
+++ calculator.py
@@ -3,29 +3,18 @@ def add(x, y):
     return x + y
 
 
-
-
-
 def subtract(x, y):
     return x - y
-
-
 
 
 def multiplication(x, y):
     return x * y
 
 
-
-
 def division(x, y):
     if y == 0:
         return "0 cannot be processed"
     return x / y  
-
-
-
-
 
 
 print("--------------------------------------------")
